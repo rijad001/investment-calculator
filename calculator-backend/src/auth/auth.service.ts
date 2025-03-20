316bec0c6fc397d3f24b163520def3fe589da85b
rimamovic <rimamovic@capeannenterprises.com>
2025-03-20 09:41:01 +0100

    Removed update button

---
+++ calculator-backend/src/auth/auth.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { UsersRepository } from './users.repository';
+import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private jwtService: JwtService,
+  ) {}
+
+  async signUp(authCredentials: AuthCredentialsDto): Promise<void> {
+    await this.usersRepository.createUser(authCredentials);
+  }
+
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.usersRepository.findOne(username);
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload: JwtPayload = { username };
+      const accessToken: string = await this.jwtService.sign(payload);
+      return { accessToken };
+    } else {
+      throw new UnauthorizedException('Please check your login credentials');
+    }
+  }
+}
--- calculator-backend/src/main.ts
+++ calculator-backend/src/main.ts
@@ -1,13 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'http://localhost:4200', // Allow requests from frontend
     methods: 'GET,POST,PUT,DELETE', // Allow these HTTP methods
-    credentials: true // Allow cookies if needed
+    credentials: true, // Allow cookies if needed
   });
+
+  // Dodaj globalni ValidationPipe
+  /*  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Uklanja nepoznata polja iz DTO-ova
+      forbidNonWhitelisted: true, // Baca grešku ako se pojavi nepoznato polje
+      transform: true, // Automatski transformira ulazne podatke u odgovarajuće tipove
+    }),
+  ); */
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
